b67387072aacf0311ad2095d1fa49afc74fc0bcd
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-02-16 23:21:27 +0530

    Time: 6 ms (40.67%), Space: 53.2 MB (5.34%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        Arrays.sort(nums);
+        int maxoccurence=0;
+        int majorityelement=0;
+        for(int i=0;i<nums.length;i++){
+            int current=nums[i];
+            int j=i+1;
+            while(j<nums.length && nums[j]==current){
+                j++;
+            }
+            
+            int currentoccurence=j-i;
+            if(currentoccurence>maxoccurence){
+                majorityelement=current;
+                maxoccurence=currentoccurence;
+            }
+            i=j-1;
+            
+        }
+        return majorityelement;
+        
+    }
+}
