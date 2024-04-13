9b00382fa14c24afa98ffed6a93d752638d829c0
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-04-13 23:54:30 +0530

    Time: 26 ms (27.95%), Space: 45.6 MB (73.97%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,75 @@
+import java.util.Stack;
+
+class Solution {
+    public int maxHistogram(int[] heights) {
+        int maxarea = 0;
+        int nsr[] = new int[heights.length];
+        int nsl[] = new int[heights.length];
+
+        // for next smaller right
+        Stack<Integer> s1 = new Stack<>();
+
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!s1.isEmpty() && heights[s1.peek()] >= heights[i]) {
+                s1.pop();
+            }
+            if (s1.isEmpty()) {
+                nsr[i] = heights.length;
+            } else {
+                nsr[i] = s1.peek();
+            }
+            s1.push(i);
+        }
+
+        // for next smaller left
+        Stack<Integer> s2 = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!s2.isEmpty() && heights[s2.peek()] >= heights[i]) {
+                s2.pop();
+            }
+            if (s2.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s2.peek();
+            }
+            s2.push(i);
+        }
+        // current area
+        for (int i = 0; i < heights.length; i++) {
+            int h = heights[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currarea = h * width;
+            maxarea = Math.max(currarea, maxarea);
+        }
+        return maxarea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int maxarea = 0;
+        int[] currRow = new int[matrix[0].length];
+
+        // Convert first row to integer array
+        for (int j = 0; j < matrix[0].length; j++) {
+            currRow[j] = matrix[0][j] - '0';
+        }
+
+        maxarea = maxHistogram(currRow);
+
+        // Update and process subsequent rows
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    currRow[j] += 1;
+                } else {
+                    currRow[j] = 0;
+                }
+            }
+            int currAns = maxHistogram(currRow);
+            maxarea = Math.max(currAns, maxarea);
+        }
+        return maxarea;
+    }
+}
