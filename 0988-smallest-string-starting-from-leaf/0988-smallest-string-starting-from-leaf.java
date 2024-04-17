0b9639743eff078ab7e98359ffd5942531a8dbee
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-04-17 23:48:49 +0530

    Time: 1 ms (99.37%), Space: 44.1 MB (71.49%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -14,27 +14,30 @@
  * }
  */
 class Solution {
-     private String dfs(TreeNode node, String current) {
+     private void dfs(TreeNode node, StringBuilder current, StringBuilder smallest) {
         if (node == null)
-            return null;
+            return;
 
-        current = (char) ('a' + node.val) + current;
+        current.append((char) ('a' + node.val));
 
         if (node.left == null && node.right == null) { // Leaf node
-            return current;
+            current.reverse(); // Reverse the current string to get the string from leaf to root
+            String currentString = current.toString();
+            if (smallest.length() == 0 || currentString.compareTo(smallest.toString()) < 0) {
+                smallest.setLength(0);
+                smallest.append(currentString);
+            }
+            current.reverse(); // Re-reverse the current string to maintain the original order
+        } else {
+            dfs(node.left, current, smallest);
+            dfs(node.right, current, smallest);
         }
 
-        String leftSmallest = dfs(node.left, current);
-        String rightSmallest = dfs(node.right, current);
-
-        if (leftSmallest == null)
-            return rightSmallest;
-        if (rightSmallest == null)
-            return leftSmallest;
-
-        return leftSmallest.compareTo(rightSmallest) < 0 ? leftSmallest : rightSmallest;
+        current.deleteCharAt(current.length() - 1); // Backtrack
     }
     public String smallestFromLeaf(TreeNode root) {
-        return dfs(root, "");
+        StringBuilder smallest = new StringBuilder();
+        dfs(root, new StringBuilder(), smallest);
+        return smallest.toString();
     }
 }
