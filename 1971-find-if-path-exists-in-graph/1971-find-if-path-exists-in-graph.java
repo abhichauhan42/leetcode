5e188451d33299e0a661d9b8497c1d058a3ee93a
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-04-21 23:31:20 +0530

    Time: 158 ms (34.49%), Space: 158.7 MB (31.91%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-       Map<Integer, List<Integer>> graph = new HashMap<>();
+              Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int[] edge : edges) {
             graph.computeIfAbsent(edge[0], key -> new ArrayList<>()).add(edge[1]);
             graph.computeIfAbsent(edge[1], key -> new ArrayList<>()).add(edge[0]);
