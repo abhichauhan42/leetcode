ea58a093cf7c49b953f672ddadd9f345f9a75b52
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-04-21 23:29:11 +0530

    Time: 170 ms (32.79%), Space: 158.8 MB (31.80%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+       Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int[] edge : edges) {
             graph.computeIfAbsent(edge[0], key -> new ArrayList<>()).add(edge[1]);
             graph.computeIfAbsent(edge[1], key -> new ArrayList<>()).add(edge[0]);
         }
-        boolean[] visited = new boolean[n];
-        return dfs(graph, source, destination, visited);
-    }
 
-    private boolean dfs(Map<Integer, List<Integer>> graph, int source, int destination, boolean[] visited) {
-        if (source == destination)
-            return true;
-        visited[source] = true;
-        List<Integer> neighbors = graph.getOrDefault(source, new ArrayList<>());
-        for (int neighbor : neighbors) {
-            if (!visited[neighbor] && dfs(graph, neighbor, destination, visited))
+        Set<Integer> visited = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(source);
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            if (current == destination)
                 return true;
+            visited.add(current);
+            List<Integer> neighbors = graph.getOrDefault(current, new ArrayList<>());
+            for (int neighbor : neighbors) {
+                if (!visited.contains(neighbor))
+                    stack.push(neighbor);
+            }
         }
+
         return false;
     }
 }
