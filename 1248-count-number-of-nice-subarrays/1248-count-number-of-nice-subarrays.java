ebf7a27826031d71e25a2463a256fe31bb175afc
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-06-22 11:27:08 +0530

    Time: 13 ms (51.25%), Space: 54.6 MB (88.96%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -5,26 +5,23 @@ class Solution {
 
     // Helper function to calculate the number of subarrays with at most K odd numbers
     private int atMostK(int[] nums, int k) {
-        int start = 0;
-        int oddCount = 0;
-        int result = 0;
+        int start = 0, end = 0;
+        int oddCount = 0, result = 0;
 
-        for (int end = 0; end < nums.length; end++) {
-            // Increment oddCount if current number is odd
+        while (end < nums.length) {
             if (nums[end] % 2 != 0) {
                 oddCount++;
             }
-
-            // If there are more than k odd numbers, move the start pointer
+            
             while (oddCount > k) {
                 if (nums[start] % 2 != 0) {
                     oddCount--;
                 }
                 start++;
             }
-
-            // All subarrays between start and end are valid
+            
             result += end - start + 1;
+            end++;
         }
 
         return result;
