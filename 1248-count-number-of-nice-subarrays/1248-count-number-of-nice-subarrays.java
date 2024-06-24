4a9af2c8d9287dba2b3c173c7eb3699440df939f
Abhishek chauhan <114762445+abhichauhan42@users.noreply.github.com>
2024-06-24 11:56:51 +0530

    Time: 42 ms (15.13%), Space: 55.3 MB (13.96%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,29 +1,25 @@
 class Solution {
    public int numberOfSubarrays(int[] nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
+       HashMap<Integer, Integer> map = new HashMap<>();
+       
+       int result = 0;
+       int oddcount = 0;
+       
+       map.put(0, 1); 
+       int n = nums.length;
+       
+       for (int i = 0; i < n; i++) {
+           if (nums[i] % 2 == 1) {
+               oddcount++;
+           }
+           
+           if (map.containsKey(oddcount - k)) {
+               result += map.get(oddcount - k);
+           }
+           
+           map.put(oddcount, map.getOrDefault(oddcount, 0) + 1);
+       }
+       return result;
     }
 
-    // Helper function to calculate the number of subarrays with at most K odd numbers
-    private int atMostK(int[] nums, int k) {
-        int start = 0, end = 0;
-        int oddCount = 0, result = 0;
-
-        while (end < nums.length) {
-            if (nums[end] % 2 != 0) {
-                oddCount++;
-            }
-            
-            while (oddCount > k) {
-                if (nums[start] % 2 != 0) {
-                    oddCount--;
-                }
-                start++;
-            }
-            
-            result += end - start + 1;
-            end++;
-        }
-
-        return result;
-    }
 }
